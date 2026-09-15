15caf5f12c2435733253572aa5467b0fdff4348a
AllisonSantos166 <allisongamer454@gmail.com>
2026-09-15 16:46:48 -0300

    add: code in main.js

--- main.js
+++ main.js
@@ -16,15 +16,17 @@ alert(`Olá, bem vindo ${user}!`)
         alert(`A subtração é ${sub}`)
         alert(`A multiplicação é ${mul}`)
         alert(`A divisão é ${div}`)
-
-    let idade = +prompt("Informe sua idade: ")
-    
-    if(idade >= 18){
-        alert("Maior de idade ")
-    }
-    else {
-        alert("Menor de idade ")
-    }
+        
+        let idade = +prompt("Informe sua idade: ")
+        
+        if(idade >= 18){
+            alert("Maior de idade ")
+        }
+        else {
+            alert("Menor de idade ")
+        }
+        
+    alert("Informe um número pra descobrir se ele é ímpar ou par:")
     
     let numero = +prompt("Informe um número: ")
     
@@ -34,6 +36,8 @@ alert(`Olá, bem vindo ${user}!`)
     else {
         alert("O número é impar")
     }
+    
+    alert("Informe três números para ver qual é o maior:")
     
     let prim_numero = +prompt("Informe o primeiro número: ")
     let segun_numero = +prompt("Informe o segundo número: ")
@@ -56,3 +60,91 @@ alert(`Olá, bem vindo ${user}!`)
         alert("Reprovado")
     }
     
+    const LIMITE_PARA_DESCONTO = 5
+    const PORCENTAGEM_DESCONTO = 0.10
+    
+    
+    let preco = parseFloat(prompt("Digite o preço do produto:"))
+    let quantidade = parseInt(prompt("Digite a quantidade desejada:"))
+    
+    let valorTotal = preco * quantidade
+    
+    if (quantidade > LIMITE_PARA_DESCONTO) {
+        let valorDoDesconto = valorTotal * PORCENTAGEM_DESCONTO;
+        valorTotal = valorTotal - valorDoDesconto
+        
+        alert(`Oba! Como você comprou mais de ${LIMITE_PARA_DESCONTO} unidades, ganhou 10% de desconto.\nO valor final a pagar é: R$ ${valorTotal.toFixed(2)}`)
+    }
+    else {
+        alert(`O valor final a pagar é: R$ ${valorTotal.toFixed(2)}`)
+    }
+    
+  alert("Digite dois números e o operador pra ver o resultado")
+  
+  let primeiro_numero = +prompt("Informe o primeiro número: ")
+  let segundo_numero = +prompt("Informe o segundo número: ")
+  let operador = prompt("Informe o operador (+, -, *, /): ")
+  
+  let total
+  
+  if (operador === '+') {
+    total = primeiro_numero + segundo_numero
+    
+} else if (operador === '-') {
+    total = primeiro_numero - segundo_numero
+    
+} else if (operador === '*') {
+    total = primeiro_numero * segundo_numero
+    
+} else if (operador === '/') {
+    
+    if (segundo_numero === 0) {
+        total = "Erro: não é possível dividir por zero!"
+    } else {
+        total = primeiro_numero / segundo_numero
+    }
+    
+} else {
+    
+    total = "Operador inválido!"
+}
+
+alert(`Resultado: ${total}`)
+
+alert("Informe seu ano de nascimento, para saber se vc pode tirar carteira de motorista")
+let nascimento = +prompt("Seu ano de nascimento:")
+let anoAtual = new Date().getFullYear();
+let idadesla = anoAtual - nascimento
+
+if (idadesla >= 18){
+    alert("Você já pode tirar sua carteira de motorista")
+}
+else {
+    alert("Você ainda não pode tirar sua carteira de motorista")
+}
+
+alert("Coloque a temperatura em graus Celsius pra saber o clima: \n Mais de 30°C = Calor \n Até 29°C = Ameno \n Abaixo de 20°C = Frio")
+let clima = +prompt("Informe a atual temperatura em graus Celsius:")
+if (clima >= 30){
+    alert("O clima está Calor")
+}
+else if (clima >= 20){
+    alert("O clima está Ameno")
+}
+else {
+    alert("O clima está Frio")
+}
+
+alert("Informe dois números pra saber qual deles é maior, ou se são iguais:")
+let sla1 = +prompt("Informe o primeiro número:")
+let sla2 = +prompt("Informe o segundo número:")
+
+if(sla1 > sla2){
+    alert(`O primeiro número é o maior: ${sla1}`)
+}
+else if (sla1 === sla2){
+    alert("Os dois números são iguais")
+}
+else {
+    alert(`O segundo número é o maior: ${sla2}`)
+}
